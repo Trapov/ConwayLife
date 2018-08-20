d3e8498828ee9e48432a4de8701538f6bb3ad0df
dmitriy afonin <dmitriy.afonin@walletone.com>
2018-08-20 23:02:21 +0500

    RenderQueue and diagnostic header were added

--- src/ConwayLife.App.Console/Program.cs
+++ src/ConwayLife.App.Console/Program.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Drawing;
 using System.Threading.Tasks;
 using System.Collections.Generic;
+using System.Diagnostics;
 
 using Output = Colorful.Console;
 using System.Threading;
@@ -13,38 +14,69 @@ namespace ConwayLife.App.Console
 {
     public static class Program
     {
-        public static void Main(string[] args)
+        private static readonly RenderQueue RenderQueue = new RenderQueue();
+
+        public static void Main()
         {
             var sourceToken = new CancellationTokenSource();
-            var task = Task.Run(() =>
+            Task.Run(() =>
             {
-                var world = new World(3);
+                var world = new World(9);
 
                 var generation = world.Generation(new List<Coordinate>()
                 {
                     new Coordinate(1, 2),
                     new Coordinate(2, 2),
-                    new Coordinate(3, 2)
-                }).ToList();
+                    new Coordinate(3, 2),
 
-                Task renderTask = Task.CompletedTask;
+                    new Coordinate(4, 5),
+                    new Coordinate(5, 5),
+                    new Coordinate(6, 5),
+
+                    new Coordinate(0, 6),
+                    new Coordinate(0, 7),
+                    new Coordinate(1, 6),
+                    new Coordinate(1, 7),
+
+                }).ToList().AsReadOnly();
+
                 while (!sourceToken.IsCancellationRequested)
                 {
-                    generation = world.Generation(generation).ToList();
-                    renderTask.GetAwaiter().GetResult();
-                    renderTask = Task.Run(() => RenderGrid(4, generation.AsReadOnly()));
-                    Task.Delay(700).GetAwaiter().GetResult();
+                    if (RenderQueue.Count > 25) continue;
+
+                    generation = world.Generation(generation).ToList().AsReadOnly();
+                    RenderQueue.Add(() => RenderGrid(9, generation), sourceToken.Token);
                 }
             }, sourceToken.Token);
 
+            RenderQueue.RenderAsync(sourceToken.Token, TimeSpan.FromMilliseconds(360));
+
             Output.ReadLine();
             sourceToken.Cancel();
-            task.GetAwaiter().GetResult();
         }
 
-        public static void RenderGrid(int gridSize, IReadOnlyCollection<Coordinate> coordinates)
+        private static int GetWorkingThreads()
+        {
+            ThreadPool.GetMaxThreads(out var maxThreads, out _);
+
+            ThreadPool.GetAvailableThreads(out var availableThreads, out _);
+
+            return maxThreads - availableThreads;
+        }
+
+        private static void RenderGrid(int gridSize, IReadOnlyCollection<Coordinate> coordinates)
         {
             Output.Clear();
+
+            Output.WriteLine(" ProcessName: " + Process.GetCurrentProcess()
+                .ProcessName, Color.Aquamarine);
+            Output.WriteLine(" ProcessId: " + Process.GetCurrentProcess()
+                .Id, Color.Aquamarine);
+            Output.WriteLine(" WorkingSet64: " + Process.GetCurrentProcess()
+                .WorkingSet64*0.000001 + " Mb", Color.Aquamarine);
+            Output.WriteLine(" Threads: " + GetWorkingThreads(), Color.Aquamarine);
+            Output.WriteLine();
+
             for (var y = 0; y<gridSize; y++)
             {
                 for (var x = 0; x < gridSize; x++)
